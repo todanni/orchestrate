9f9ccf13560a4007036a4a58defc6abd49904805
Danni Popova <dannipopova@improbable.io>
2021-07-23 19:32:29 +0100

    create one docker file for droplet

---
+++ database/init.sql
@@ -0,0 +1,8 @@
+CREATE USER development;
+CREATE DATABASE development;
+GRANT ALL PRIVILEGES ON DATABASE development TO development;
+
+
+create database production;
+CREATE DATABASE production;
+GRANT ALL PRIVILEGES ON DATABASE production TO production;
