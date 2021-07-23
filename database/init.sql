2dfdab2b97752d748f1b2d686ce5e7d009edf515
Danni Popova <dannipopova@improbable.io>
2021-07-23 19:51:37 +0100

    update deploy jobs

--- database/init.sql
+++ database/init.sql
@@ -3,6 +3,6 @@ CREATE DATABASE development;
 GRANT ALL PRIVILEGES ON DATABASE development TO development;
 
 
-create database production;
+create USER production;
 CREATE DATABASE production;
 GRANT ALL PRIVILEGES ON DATABASE production TO production;
